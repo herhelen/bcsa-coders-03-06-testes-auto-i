1ce75a96c120d04b69ee59818bb323557ee11ea9
Helen Her <her.helen@gmail.com>
2023-11-17 20:41:09 -0300

    class 7: using mock instead of dummy

---
+++ ada-banco-tdd/src/test/java/com/ada/banco/domain/usecase/CriarNovaContaDummyTest.java
@@ -0,0 +1,36 @@
+package com.ada.banco.domain.usecase;
+
+import com.ada.banco.domain.gateway.ContaGateway;
+import com.ada.banco.domain.model.Conta;
+import com.ada.banco.dummy.ContaGatewayDummyImpl;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class CriarNovaContaDummyTest {
+
+    @Test
+    public void deveLancarUmaExceptionCasoUsuarioJaTemUmaConta() {
+        // given
+        ContaGateway contaGateway = new ContaGatewayDummyImpl(); // Responsável por verificar se existe uma conta
+        CriarNovaConta criarNovaConta = new CriarNovaConta(contaGateway);
+        Conta conta = new Conta(2222L, 2L, 1L, BigDecimal.ZERO, "Paula", "11133355500");
+
+        // when & then
+        assertThrows(Exception.class,
+                () -> criarNovaConta.execute(conta));
+    }
+
+    @Test
+    public void deveCriarUmaContaNovaComSucesso() throws Exception {
+        // given
+        ContaGateway contaGateway = new ContaGatewayDummyImpl(); // Responsável por verificar se existe uma conta
+        CriarNovaConta criarNovaConta = new CriarNovaConta(contaGateway);
+        Conta conta = new Conta(3333L, 2L, 1L, BigDecimal.ZERO, "Elsa", "22211155500");
+
+        // when & then
+        Conta novaConta = criarNovaConta.execute(conta);
+        assertEquals(conta, novaConta);
+    }
+}
--- ada-banco-tdd/src/test/java/com/ada/banco/domain/usecase/CriarNovaContaTest.java
+++ ada-banco-tdd/src/test/java/com/ada/banco/domain/usecase/CriarNovaContaTest.java
@@ -2,35 +2,47 @@ package com.ada.banco.domain.usecase;
 
 import com.ada.banco.domain.gateway.ContaGateway;
 import com.ada.banco.domain.model.Conta;
-import com.ada.banco.dummy.ContaGatewayDummyImpl;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CriarNovaContaTest {
 
     @Test
-    public void deveLancarUmaExceptionCasoUsuarioJaTemUmaConta() {
-        // given
-        ContaGateway contaGateway = new ContaGatewayDummyImpl(); // Responsável por verificar se existe uma conta
-        CriarNovaConta criarNovaConta = new CriarNovaConta(contaGateway);
-        Conta conta = new Conta(2222L, 2L, 1L, BigDecimal.ZERO, "Paula", "11133355500");
-
-        // when & then
-        assertThrows(Exception.class,
-                () -> criarNovaConta.execute(conta));
-    }
-
-    @Test
     public void deveCriarUmaContaNovaComSucesso() throws Exception {
         // given
-        ContaGateway contaGateway = new ContaGatewayDummyImpl(); // Responsável por verificar se existe uma conta
+        ContaGateway contaGateway = mock(ContaGateway.class);
         CriarNovaConta criarNovaConta = new CriarNovaConta(contaGateway);
         Conta conta = new Conta(3333L, 2L, 1L, BigDecimal.ZERO, "Elsa", "22211155500");
 
         // when & then
+        when(contaGateway.buscarPorCpf(conta.getCpf())).thenReturn(null);
+        when(contaGateway.salvar(conta)).thenReturn(conta);
+
         Conta novaConta = criarNovaConta.execute(conta);
         assertEquals(conta, novaConta);
+
+        verify(contaGateway, times(1)).buscarPorCpf(conta.getCpf());
+        verify(contaGateway, times(1)).salvar(conta);
+    }
+
+    @Test
+    public void deveLancarUmaExceptionCasoUsuarioJaTemUmaConta() {
+        // given
+        ContaGateway contaGateway = mock(ContaGateway.class);
+        CriarNovaConta criarNovaConta = new CriarNovaConta(contaGateway);
+        Conta conta = new Conta(2222L, 2L, 1L, BigDecimal.ZERO, "Paula", "11133355500");
+
+        // when & then
+        when(contaGateway.buscarPorCpf(conta.getCpf())).thenReturn(conta);
+        assertThrows(Exception.class,
+                () -> criarNovaConta.execute(conta));
+
+        verify(contaGateway, times(1)).buscarPorCpf(conta.getCpf());
+        verify(contaGateway, never()).salvar(conta);
     }
 }
